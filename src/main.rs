55d7d9c74acaee6aebedbe4456dabffa6ac2cbe2
Armin Häberling <armin.aha@gmail.com>
2016-05-11 22:44:45 +0200

    Read MAC address as command line input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,48 @@
 
 extern crate magic_packet;
 
+extern crate clap;
+extern crate regex;
+
+use clap::{Arg, App};
+use regex::Regex;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    let mac = [1, 2, 3, 4, 5, 6];
+    let matches = App::new("magic-packet")
+                        .version(VERSION)
+                        .arg(Arg::with_name("MAC_ADDR")
+                                .help("The MAC address of the target")
+                                .required(true)
+                                .index(1)
+                                .validator(is_mac))
+                        .get_matches();
+    let mac = matches.value_of("MAC_ADDR").unwrap();
+    let mac = read_mac(mac);
     let address = "192.168.1.255:9";
     magic_packet::send_magic_packet(&mac, address).unwrap();
+}
+
+fn is_mac(val: String) -> Result<(), String> {
+    let mac_regex = Regex::new("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$").unwrap();
+    if mac_regex.is_match(&val) {
+        Ok(())
+    } else {
+        Err("\"".to_string() + &val + "\" is not a MAC address.")
+    }
+}
+
+fn read_mac(val: &str) -> [u8; 6] {
+    let mac_regex = Regex::new("^([0-9A-Fa-f]{2})[:-]([0-9A-Fa-f]{2})[:-]([0-9A-Fa-f]{2})[:-]\
+                        ([0-9A-Fa-f]{2})[:-]([0-9A-Fa-f]{2})[:-]([0-9A-Fa-f]{2})$").unwrap();
+    let caps = mac_regex.captures(val).unwrap();
+    let items = (1..7).map(|x| caps.at(x).unwrap())
+                      .map(|x| u8::from_str_radix(x, 16).unwrap())
+                      .collect::<Vec<_>>();
+    let mut mac = [0u8; 6];
+    for i in 0..6 {
+        mac[i] = items[i];
+    }
+    mac
 }

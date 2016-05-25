5db2e4d30a454329555ee961366e81663d2df9c5
Armin Häberling <armin.aha@gmail.com>
2016-05-25 21:44:33 +0200

    Fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -21,29 +21,26 @@ pub fn send_magic_packet<A: ToSocketAddrs>(mac: &[u8; 6], addr: A) -> Result<(),
 }
 
 #[cfg(test)]
-#[test]
-fn create_valid_magic_packet() {
-    let mac = [1, 2, 3, 4, 5, 6];
-    let buffer = create_magic_packet(&mac);
-    println!("{:?}", &buffer[..]);
-    assert_eq!(102, buffer.len());
-    assert_eq!(255, buffer[3]);
-    assert_eq!(1, buffer[6]);
-    assert_eq!(3, buffer[8]);
-    assert_eq!(6, buffer[11]);
-    assert_eq!(1, buffer[96]);
-    assert_eq!(3, buffer[98]);
-    assert_eq!(6, buffer[101]);
-}
-
-#[cfg(test)]
 mod test {
 
-    use magic;
+    #[test]
+    fn create_valid_magic_packet() {
+        let mac = [1, 2, 3, 4, 5, 6];
+        let buffer = super::create_magic_packet(&mac);
+        println!("{:?}", &buffer[..]);
+        assert_eq!(102, buffer.len());
+        assert_eq!(255, buffer[3]);
+        assert_eq!(1, buffer[6]);
+        assert_eq!(3, buffer[8]);
+        assert_eq!(6, buffer[11]);
+        assert_eq!(1, buffer[96]);
+        assert_eq!(3, buffer[98]);
+        assert_eq!(6, buffer[101]);
+    }
 
     #[test]
     fn broadcast_magic_packet() {
         let mac = [1, 2, 3, 4, 5, 6];
-        assert_eq!((), magic::send_magic_packet(&mac, "192.168.1.255:9").unwrap());
+        assert_eq!((), super::send_magic_packet(&mac, "192.168.1.255:9").unwrap());
     }
 }

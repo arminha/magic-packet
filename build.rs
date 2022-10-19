cb93306a4ea18fa558a8a0d7e0777dbf62972232
Armin Häberling <armin.aha@gmail.com>
2022-10-19 11:11:17 +0200

    Use Rust 2021 edition

--- build.rs
+++ build.rs
@@ -14,8 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>
 */
-extern crate clap;
-extern crate regex;
 
 use clap::Shell;
 use std::env;
--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>
 */
 #![forbid(unsafe_code)]
 
-extern crate clap;
-extern crate regex;
-
 use regex::Regex;
 
 mod cli;
